d9feb7e3db03cbdaec17f1cd1a422fa53e683fa4
MadhuriKsh <msksh2096@gmail.com>
2022-05-31 22:58:24 +0530

    [Madhuri]: added employee attendance in employee EmployeeWageProblem.

---
+++ empWageComputation.sh
@@ -0,0 +1,12 @@
+#UC1 Started
+
+#!/bin/bash
+isPresent=1
+salary=0
+randomcheck=$(( $RANDOM%2 ))
+if [ $randomcheck -eq $isPresent ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
